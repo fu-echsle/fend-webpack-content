d77fb6b2938975f40400a82798367957e7320950
Yvonne Vogt <fu-echsle@siebenelf.com>
2021-04-27 08:16:28 +0200

    + Adding some loaders
    - Removing package-lock.json as I'm using yarn

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
+import { handleSubmit } from './js/formHandler';
+import { checkForName} from "./js/nameChecker";
+
+console.log(handleSubmit())
+console.log(checkForName())
 alert('I exist');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
-const path = require('path') 
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [new ESLintPlugin()]
 }
