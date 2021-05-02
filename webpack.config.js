466a8732f3892b81f9c3436f217a0613136c1065
Yvonne Vogt <fu-echsle@siebenelf.com>
2021-05-02 15:40:07 +0200

    + added plugin to inject js to html

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/client/index.js',
-    devtool: 'source-map',
     module: {
         rules: [
             {
                 test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new ESLintPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
