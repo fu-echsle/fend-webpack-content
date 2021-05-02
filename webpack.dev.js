2093419ae25d9850077e6105927ecfaf2baa7ad8
Yvonne Vogt <fu-echsle@siebenelf.com>
2021-05-02 19:24:07 +0200

    ~ Fetching stuff from the previous branches
    ~ Updating outdated stuff
    + Adding webpack serve

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
@@ -20,5 +20,9 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         })
-    ]
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
 }
